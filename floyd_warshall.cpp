66ad0b0cf57dca52605b262a78a2aedfe3a0cc83
Ahashan_Akash <163560125+Ahashanakash@users.noreply.github.com>
2024-12-24 10:28:37 +0600

    final

--- floyd_warshall.cpp
+++ floyd_warshall.cpp
@@ -1,59 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int, int>> graph[1005];
-int dis[1005];
-class cmp
+#define V 4
+
+#define INF 99999
+
+void printSolution(int dist[][V]);
+void floydWarshall(int dist[][V])
 {
-public:
-    bool operator()(pair<int, int> a, pair<int, int> b)
-    {
-        return a.second > b.second;
-    }
-};
-void dijkstra(int src)
-{
-    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
-    pq.push({src, 0});
-    dis[src] = 0;
-    while (!pq.empty())
-    {
-        pair<int, int> parent = pq.top();
-        pq.pop();
-        int node = parent.first;
-        int cost = parent.second;
-        for (pair<int, int> child : graph[node])
-        {
-            int childNode = child.first;
-            int childCost = child.second;
-            if (cost + childCost < dis[childNode])
-            {
-                // path relax
-                dis[childNode] = cost + childCost;
-                pq.push({childNode, dis[childNode]});
+    int i, j, k;
+
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][j] > (dist[i][k] + dist[k][j])
+                    && (dist[k][j] != INF
+                        && dist[i][k] != INF))
+                    dist[i][j] = dist[i][k] + dist[k][j];
             }
         }
     }
+
+    printSolution(dist);
 }
+
+void printSolution(int dist[][V])
+{
+    cout << "The following matrix shows the shortest "
+            "distances"
+            " between every pair of vertices \n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF"
+                     << " ";
+            else
+                cout << dist[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
+
+// Driver's code
 int main()
 {
-    int n, e;
-    cin >> n >> e;
-    while (e--)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        graph[a].push_back({b, c});
-        graph[b].push_back({a, c});
-    }
-    for (int i = 0; i < n; i++)
-    {
-        dis[i] = INT_MAX;
-    }
-    dijkstra(0);
-    for (int i = 0; i < n; i++)
-    {
-        cout << i << " -> " << dis[i] << endl;
-    }
+    int graph[V][V] = { { 0, 5, INF, 10 },
+                        { INF, 0, 3, INF },
+                        { INF, INF, 0, 1 },
+                        { INF, INF, INF, 0 } };
+
+    floydWarshall(graph);
     return 0;
 }
